89ebfb0723c8d146454382315c5a19363bfab9d8
MarcelDevFagbohoun <houenoumarcel897@gmail.com>
2024-10-18 15:15:00 +0200

    le premier commit des exercices cisco

---
+++ exo1.js
@@ -0,0 +1,49 @@
+/*let newData =[
+    {
+        name:"Juliane Stace",
+        studentClass: "1st Year university",
+        course:"Agriculture",
+        age:24
+    },
+    {
+        name:"Jubril Soft",
+        studentClass:"Gradruate 2nd",
+        course:"Computer Science",
+        age:27,
+    },
+    {
+        name:"Ezekiel Joshua",
+        studentClass:"Junior secondary 2",
+        course:"French Studies",
+        age:15,
+    }
+]
+console.log(typeof(newData));
+console.log(newData instanceof Array);
+console.log(newData[0].name instanceof String)
+console.log(delete newData[0].age)
+console.log(newData.length)
+console.log(newData[0]["age"]="24")
+console.log(typeof newData[0].age)
+console.log(`the first student name is: ${newData[0].name} and his age is ${newData[0].age} he studies ${newData[0].course} `)
+console.log(`the second student name is: ${newData[1].name} and his age is ${newData[1].age} he studies ${newData[1].course} `)
+const julianAge=34;
+let size= (julianAge>=18) ? "Julien est majuer": "il est mineur";
+console.log(size);
+
+let name;
+if(test){
+    name="Alice";
+}
+else{
+    name="Bob";
+}
+if (test )  ? name="Alice":name="Bob";*/
+let n=4;
+let i=10;
+while( i>=1)
+{
+    let res= n*i;
+    console.log(`${n} * ${i} = ${res}`);
+    i--
+}
